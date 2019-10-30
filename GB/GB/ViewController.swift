555c2d70aee521490eb6e646ad2dc5bd3d1f2f84
Денис Сидоренко <dvsidorenko92@gmail.com>
2019-10-30 10:31:28 +0300

    create project

---
+++ GB/GB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GB
+//
+//  Created by Сидоренко Денис Викторович on 30/10/2019.
+//  Copyright © 2019 Сидоренко Денис Викторович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
